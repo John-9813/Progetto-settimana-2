e11d358eaeb3e65308ea29383f4a1b6a51485e2d
John-9813 <john.oliver98.br@gmail.com>
2024-06-21 18:55:20 +0200

    conclusione JS

--- JS Exercises/D5.js
+++ JS Exercises/D5.js
@@ -38,7 +38,7 @@ console.log(pets);
 console.log("------esercizio 4-------");
 
 pets.push(pets.shift());
-console.log(pets)
+console.log(pets);
 
 /* ESERCIZIO 5
     Dato il seguente array di oggetti, scrivi del codice per aggiungere ad ognuno di essi una proprietà "licensePlate" con valore a tua scelta.
@@ -66,7 +66,7 @@ const cars = [
   },
 ];
 
-cars.forEach(cars => {
+cars.forEach((cars) => {
   cars.licensePlate = "ABC123";
 });
 
@@ -86,7 +86,7 @@ const newCar = {
 };
 cars.push(newCar);
 
-cars.forEach(cars => {
+cars.forEach((cars) => {
   cars.trims.pop();
 });
 
@@ -99,11 +99,25 @@ console.log("------esercizio 7-------");
 
 const justTrims = [];
 
+cars.forEach((cars) => {
+  justTrims.push(cars.trims[0]);
+});
+
+console.log(justTrims);
+
 /* ESERCIZIO 8
     Cicla l'array "cars" e costruisci un if/else statament per mostrare due diversi messaggi in console. Se la prima lettera della proprietà
     "color" ha valore "b", mostra in console "Fizz". Altrimenti, mostra in console "Buzz".
 */
 console.log("------esercizio 8-------");
+
+cars.forEach((car) => {
+  if (car.color.charAt(0) === "b") {
+    console.log("Fizz");
+  } else {
+    console.log("Buzz");
+  }
+});
 
 /* ESERCIZIO 9
     Utilizza un ciclo while per stampare in console i valori del seguente array numerico fino al raggiungimento del numero 32.
@@ -114,6 +128,15 @@ const numericArray = [
   6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105,
 ];
 
+// console.log(numericArray)
+
+let i = 0;
+
+while (numericArray[i] <= 32) {
+  console.log(numericArray[i]);
+  i++;
+}
+
 /* ESERCIZIO 10
     Partendo dall'array fornito e utilizzando un costrutto switch, genera un nuovo array composto dalle posizioni di ogni carattere all'interno
     dell'alfabeto italiano.
@@ -122,3 +145,88 @@ const numericArray = [
 console.log("------esercizio 10-------");
 
 const charactersArray = ["g", "n", "u", "z", "d"];
+
+charactersArray.forEach(char => {
+  switch (char) {
+    case "a":
+      console.log(1);
+      break;
+    case "b":
+      console.log(2);
+      break;
+    case "c":
+      console.log(3);
+      break;
+    case "d":
+      console.log(4);
+      break;
+    case "e":
+      console.log(5);
+      break;
+    case "f":
+      console.log(6);
+      break;
+    case "g":
+      console.log(7);
+      break;
+    case "h":
+      console.log(8);
+      break;
+    case "i":
+      console.log(9);
+      break;
+    case "j":
+      console.log(10);
+      break;
+    case "k":
+      console.log(11);
+      break;
+    case "l":
+      console.log(12);
+      break;
+    case "m":
+      console.log(13);
+      break;
+    case "n":
+      console.log(14);
+      break;
+    case "o":
+      console.log(15);
+      break;
+    case "p":
+      console.log(16);
+      break;
+    case "q":
+      console.log(17);
+      break;
+    case "r":
+      console.log(18);
+      break;
+    case "s":
+      console.log(19);
+      break;
+    case "t":
+      console.log(20);
+      break;
+    case "u":
+      console.log(21);
+      break;
+    case "v":
+      console.log(22);
+      break;
+    case "w":
+      console.log(23);
+      break;
+    case "x":
+      console.log(24);
+      break;
+    case "y":
+      console.log(25);
+      break;
+    case "z":
+      console.log(26);
+      break;
+    default:
+      console.log("Carattere non valido");
+  }
+});
